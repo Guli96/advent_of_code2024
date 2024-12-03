e392931cfb408e8a93d24905fc441ba9789e2bb4
Guli96 <guncs96@gmail.com>
2024-12-03 22:53:53 +0100

    removed logging from helper

--- santas_helper.py
+++ santas_helper.py
@@ -1,6 +1,6 @@
 def get_input_lines(day: int, demo = False):
     filepath = f"input\\{str(day).rjust(2, '0')}"
-    print(filepath)
+
     if demo:
         filepath += "_d"
     file = open(filepath)
